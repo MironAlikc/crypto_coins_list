2693098c174d9e6b69695376078f7f9ed8644fb4
Aleksandr Myroniuk <miron.alikc@gmail.com>
2023-11-09 13:22:41 -0800

    Architecture

---
+++ lib/features/crypto_coin/view/crypto_coin_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CryptoCoinScreen extends StatefulWidget {
+  const CryptoCoinScreen({super.key});
+
+  @override
+  State<CryptoCoinScreen> createState() => _CryptoCoinScreenState();
+}
+
+class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
+  String? coinName;
+  @override
+  void didChangeDependencies() {
+    final args = ModalRoute.of(context)?.settings.arguments;
+    assert(args != null && args is String, 'You must provie Strings arge');
+    // if (args == null) {
+    //   log('you must provie arge');
+    //   return;
+    // }
+    // if (args is! String) {
+    //   log('You must provie Strings arge');
+    // }
+    coinName = args as String?;
+    setState(() {});
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(coinName ?? '...'),
+      ),
+    );
+  }
+}
