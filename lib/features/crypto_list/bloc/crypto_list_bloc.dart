f947759c8f82b3af2e4e8d17ffe44f9195fc1cdb
Aleksandr Myroniuk <miron.alikc@gmail.com>
2023-11-15 02:10:17 -0800

    Talker

--- lib/features/crypto_list/bloc/crypto_list_bloc.dart
+++ lib/features/crypto_list/bloc/crypto_list_bloc.dart
@@ -4,6 +4,8 @@ import 'package:crypto_coins_list/repositories/crypto_coins/abstarct_coins_repos
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 part 'crypto_list_event.dart';
 part 'crypto_list_state.dart';
@@ -16,14 +18,20 @@ class CryptoListBloc extends Bloc<CryptoListEvent, CryptoListState> {
           emit(CryptoListLoading());
         }
         final coinList = await coinRepository.getCoinsList();
+
         emit(CryptoListLoaded(coinsList: coinList));
-        // emit(CryptoListLoaded(coinList: coinList));
-      } catch (e) {
+      } catch (e, st) {
         emit(CryptoListLoadingFailure(exception: e));
+        GetIt.I<Talker>().handle(e, st);
       } finally {
         event.completer?.complete();
       }
     });
+  }
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    GetIt.I<Talker>().handle(error, stackTrace);
   }
 
   final AbstractCoinsRepository coinRepository;
