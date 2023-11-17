a84c33e8b2ce21973740ed24fdff6430206059df
Aleksandr Myroniuk <miron.alikc@gmail.com>
2023-11-17 04:07:19 -0800

    json_serializable

--- lib/repositories/crypto_coins/crypto_coins_repository.dart
+++ lib/repositories/crypto_coins/crypto_coins_repository.dart
@@ -1,34 +1,39 @@
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
+import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin_details.dart';
 import 'package:dio/dio.dart';
 
 class CryptoCoinsRepository implements AbstractCoinsRepository {
-  CryptoCoinsRepository({required this.dio});
+  CryptoCoinsRepository({
+    required this.dio,
+  });
+
   final Dio dio;
 
   @override
   Future<List<CryptoCoin>> getCoinsList() async {
-    final response = await Dio().get(
-      "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BNB,AID,SOL,CAG,DOV&tsyms=USD",
-    );
+    final response = await dio.get(
+        'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BNB,SOL,AID,CAG,DOV&tsyms=USD');
     final data = response.data as Map<String, dynamic>;
     final dataRaw = data['RAW'] as Map<String, dynamic>;
     final cryptoCoinsList = dataRaw.entries.map((e) {
       final usdData =
           (e.value as Map<String, dynamic>)['USD'] as Map<String, dynamic>;
-      final price = usdData['PRICE'];
-      final imageUrl = usdData['IMAGEURL'];
-      return CryptoCoin(
-        name: e.key,
-        priceInUSD: price,
-        imageUrl: 'https://www.cryptocompare.com/$imageUrl',
-      );
+      final details = CryptoCoinDetail.fromJson(usdData);
+      return CryptoCoin(name: e.key, details: details);
     }).toList();
     return cryptoCoinsList;
   }
 
   @override
-  Future<CryptoCoin> getCoinDetails(String currencyCode) {
-    // TODO: implement getCoinDetails
-    throw UnimplementedError();
+  Future<CryptoCoin> getCoinDetails(String currencyCode) async {
+    final response = await dio.get(
+        'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=$currencyCode&tsyms=USD');
+    final data = response.data as Map<String, dynamic>;
+    final dataRaw = data['RAW'] as Map<String, dynamic>;
+    final coinData = dataRaw[currencyCode] as Map<String, dynamic>;
+    final usdData = coinData['USD'] as Map<String, dynamic>;
+    final details = CryptoCoinDetail.fromJson(usdData);
+
+    return CryptoCoin(name: currencyCode, details: details);
   }
 }
